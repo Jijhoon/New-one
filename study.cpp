38631c777d648dc0a2e14496477254d55c4acfcf
JiJhoon <darkmine7@inha.edu>
2024-01-24 11:50:19 +0900

    about (),[],{}

--- study.cpp
+++ study.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int main()
 {
-    int x = true; // false는 소문자로 작성!
+    int x = true; // true, false는 소문자로 작성!
     if (x != 0) // (x = 0) -> 할당연산자 but (x == 0) 일치 연산자
     { // 괄호를 항상 빼먹지 말자.
         std::cout << "statement"<<std::endl; // 세미콜론도 빼먹지 말자.
@@ -9,7 +9,21 @@ int main()
 
     if (x == 0) 
     std::cout << "statement2"<<std::endl; // 괄호가 없어도 바로 아래에 붙어있다면 작동은 하긴 한다. 여기서 중요한 것은 if block에 들어있는지 판단하는 도구가 필요한 것이다.
-    std::cout << "statement3"<<std::endl;
-        
+    std::cout << "statement3"<<std::endl; // 여기서부터는 if block에 들어있는 것이 아니라 그냥 밖에 나와있다.
+    {
+        std::cout << "statement4"<<std::endl; // --> 정상작동
+    }    
+    /*
+    (
+        std::cout << "statement5"<<std::endl;  --> 오류
+    )
+
+    [
+        std::cout << "statement5"<<std::endl; --> 오류
+    ]
+    
+    */
+
+    
     return 0; // int main()으로 시작했으니 return값이 int로 와야한다. 아마 관습적으로 0을 사용하는듯? 다른 int들도 작동은 된다.
 }
