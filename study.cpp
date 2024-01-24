692f472e7fc3feffe6f58ea08b9655d99ffeacee
JiJhoon <darkmine7@inha.edu>
2024-01-24 11:41:11 +0900

    about if

--- study.cpp
+++ study.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 int main()
 {
-    const int Size = 10;
-    // NOTE: C++에서 문자열을 입력받기 위해서는 문자열이 저장될 문자형 배열을 미리 생성해야한다. Size가 배열의 크기에 해당한다.
-    char city[Size];
-    char name[Size];
+    int x = true; // false는 소문자로 작성!
+    if (x != 0) // (x = 0) -> 할당연산자 but (x == 0) 일치 연산자
+    { // 괄호를 항상 빼먹지 말자.
+        std::cout << "statement"<<std::endl; // 세미콜론도 빼먹지 말자.
+    }
 
-    std::cout << "내가 살고 있는 곳: ";
-    std::cin.get(city, Size).ignore(Size, "\n");
-    std::cout << "이름: ";
-    std::cin.get(name, Size).ignore(Size, "\n");
-
-    std::cout << city << "에 살고," << name << "입니다." << std::endl;
-    return 0;
+    if (x == 0) 
+    std::cout << "statement2"<<std::endl; // 괄호가 없어도 바로 아래에 붙어있다면 작동은 하긴 한다. 여기서 중요한 것은 if block에 들어있는지 판단하는 도구가 필요한 것이다.
+    std::cout << "statement3"<<std::endl;
+        
+    return 0; // int main()으로 시작했으니 return값이 int로 와야한다. 아마 관습적으로 0을 사용하는듯? 다른 int들도 작동은 된다.
 }
