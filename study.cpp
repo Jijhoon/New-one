bf00ebed7d5346a1d8f587c80069b235f8e24e84
JiJhoon <darkmine7@inha.edu>
2024-01-24 16:02:45 +0900

    calculator of score

--- study.cpp
+++ study.cpp
@@ -1,18 +1,46 @@
 #include <iostream>
 int main()
 {
-    int seconds;
-    std::cout << "Press any number: ";
-    std::cin >> seconds;
-    int time = 60;
-    int hours = seconds / 3600;
-    int minuits = (seconds - (3600*hours)) / 60;
-    double second = seconds - (3600*hours) - (60*minuits);
+    float score1, score2, score3;
+    std::cout << "Write your three score\n: ";
+    std::cin >> score1,
+        std::cin >> score2,
+        std::cin >> score3;
 
-    std::cout << seconds << ": givne seconds." << std::endl;
-    std::cout << hours << ": hours." << std::endl;
-    std::cout << minuits << ": minuits." << std::endl;
-    std::cout << second << ": left-over seconds." << std::endl;
+    if (score1 && score2 && score3)
+    {
+        float arr[3] = {score1, score2, score3};
 
+        float total_score = score1 + score2 + score3;
+        int len = sizeof(arr) / sizeof(arr[0]);
+        float average = total_score / len;
+        std::cout << total_score << ": total score\n";
+        std::cout << average << ": average\n";
+        for (float element : arr)
+        {
+            std::cout << average - element << ": deviation of " << element << "\n";
+        }
+    }
+    else
+    {
+        std::cout << "You write it in worng way!";
+    }
     return 0;
 }
+
+/* Q1)check the time.
+   int seconds;
+   std::cout << "Press any number: ";
+   std::cin >> seconds;
+   int time = 60;
+   int hours = seconds / 3600;
+   int minuits = (seconds - (3600*hours)) / 60;
+   double second = seconds - (3600*hours) - (60*minuits);
+
+   std::cout << seconds << ": givne seconds." << std::endl;
+   std::cout << hours << ": hours." << std::endl;
+   std::cout << minuits << ": minuits." << std::endl;
+   std::cout << second << ": left-over seconds." << std::endl;
+*/
+
+//  Q2) average and deviation of score
