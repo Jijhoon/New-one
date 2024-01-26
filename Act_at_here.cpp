26ee97dd8494beec68311e8ec6d0b192f8fa67f3
JiJhoon <darkmine7@inha.edu>
2024-01-26 13:12:10 +0900

    class, constructor

--- Act_at_here.cpp
+++ Act_at_here.cpp
@@ -1,29 +1,79 @@
 #include <iostream>
-#include <cmath>
+#include <cassert>
 using namespace std;
+
+class Rectangle
+{
+private:
+    double length; // data member
+    double height;
+
+public:
+    Rectangle(double l, double h); // constructor
+    Rectangle(const Rectangle &r); // copy constructor
+    ~Rectangle();                  // destructor
+    void print() const;            // member function
+    double getArea() const;
+    double getPerimeter() const;
+};
+
+Rectangle ::Rectangle(double len, double hgt)
+    : length(len), height(hgt)
+
+{
+    if ((length <= 0.0) || (height <= 0.0))
+    {
+        cout << "No rectangle can be made!" << endl;
+        assert(false);
+    }
+}
+// Copy constructor
+Rectangle ::Rectangle(const Rectangle &rect)
+    : length(rect.length), height(rect.height)
+{
+}
+// Destructor
+Rectangle ::~Rectangle()
+{
+}
+// Accessor member function: Print length and height
+void Rectangle ::print() const
+{
+    cout << "A rectangle of " << length << " by " << height << endl;
+}
+// Accessor member function: Get area
+double Rectangle ::getArea() const
+{
+    return (length * height);
+}
+// Accessor member function: Get perimeter
+double Rectangle ::getPerimeter() const
+{
+    return (2 * (length + height));
+}
 int main()
 {
-    // Declations
-    const double PI = 3.14159265;
-    int n;
-    double x, y, z;
-    // Input
-    do
-    {
-        cout << "Enter the number of the function you want to use: " << endl;
-        cin >> n;
-    } while (n < 4);
-
-    do
-    {
-        cout << "Enter the value of x(Side): " << endl;
-        cin >> x;
-    } while (x <= 0.0);
-    // Process
-    y = n * x;
-    z = (n * pow(x, 2)) / (n * tan(PI / n));
-    // Output
-    cout << "The value of y(Perimeter) is: " << y << endl;
-    cout << "The value of z(Area) is: " << z << endl;
+    // Instantiation of three objects
+    Rectangle rect1(3.0, 4.2);  // Using parameter constructor
+    Rectangle rect2(5.1, 10.2); // Using parameter constructor
+    Rectangle rect3(rect2);     // Using copy constructor
+    // Operations on first rectangle
+    cout << "Rectangle 1: ";
+    rect1.print();
+    cout << "Area: " << rect1.getArea() << endl;
+    cout << "Perimeter: " << rect1.getPerimeter() << endl
+         << endl;
+    // Operations on second rectangle
+    cout << "Rectangle 2: ";
+    rect2.print();
+    cout << "Area: " << rect2.getArea() << endl;
+    cout << "Perimeter: " << rect2.getPerimeter() << endl
+         << endl;
+    // Operations on third rectangle
+    cout << "Rectangle 3: ";
+    rect3.print();
+    cout << "Area: " << rect3.getArea() << endl;
+    cout << "Perimeter: " << rect3.getPerimeter() << endl
+         << endl;
     return 0;
 }
--- study.cpp
+++ study.cpp
@@ -1,67 +1,252 @@
+/* NOTE: private, protected, public / access modifier
+private : // private member -> can't access from outside of class // class 본인만 접근 가능
+protected : // protected member -> can access from derived class // class를 상속받은 class만 접근 가능
+public : // public member -> can access from anywhere // 어디서든 접근 가능
+*/
+
+/* NOTE: classes and functions
 #include <iostream>
-#include <cmath>
+using namespace std;
+// Declaration of Circle class and its member functions
+class Circle
+{
+private:
+double radius;
+public:
+double getRadius () const; -> const member라고 정의하면 member function이 object의 data member를 수정할 수 없다.
+double getArea () const;
+double getPerimeter () const; 
+void setRadius (double value); -> const가 아니라면 member function이 object의 data member를 수정할 수 있다.
+}; <- important!!!!!!! DON'T forget semicolon
+
+// Definition of getRadius member function
+double Circle :: getRadius () const
+{
+return radius;
+}
+// Definition of getArea member function
+double Circle :: getArea () const
+{
+const double PI = 3.14;
+return (PI * radius * radius);
+}
+// Definition of getPerimeter member function
+double Circle :: getPerimeter () const
+{
+const double PI = 3.14;
+return (2 * PI * radius);
+}
+// Definition of setRadius member function
+void Circle :: setRadius (double value) // private member는 직접 접근이 불가능하므로 public member function을 정의하여 접근한다
+{
+radius = value;
+}
+
+
+int main ( )
+{
+// Creating first circle and applying member functions
+cout << "Circle 1: " << endl;
+Circle circle1;
+circle1.setRadius (10.0);
+cout << "Radius: " << circle1.getRadius() << endl;
+cout << "Area: " << circle1.getArea() << endl;
+cout << "Perimeter: " << circle1.getPerimeter() << endl << endl;
+// Creating second circle and applying member functions
+cout << "Circle 2: " << endl;
+Circle circle2;
+circle2.setRadius (20.0);
+cout << "Radius: " << circle2.getRadius() << endl;
+cout << "Area: " << circle2.getArea() << endl;
+cout << "Perimeter: " << circle2.getPerimeter();
+return 0;
+}
+*/
+
+/* NOTE: sum of number sereis
+
+     1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 ... + n = n(n+1)/2
+
+     1 + 4 + 9 + 16 + 25 + 36 + 49 + 64 + 81 + 100 ... + n^2 = n(n+1)(2n+1)/6
+
+     1 + 8 + 27 + 64 + 125 + 216 + 343 + 512 + 729 + 1000 ... + n^3 = n^2(n+1)^2/4
+
+    */
+
+/* NOTE: Sentinel-controlled loop
+
+ sentinel : a guard who prevents unauthorized person from passing a point.
+
+ -> sentinel-contlled loop : a loop that is controlled by a sentinel value.
+
+ sentinel value : a special value that is used to terminate the loop.
+
+ ex) 0, -1, 999, 100, 9999, 999999, 999999999, 999999999999999999
+
+ Programer struct the code when if -1 is entered, the loop will be terminated.
+
+ */
+
+/* NOTE: EOF-Controlled Loop
+
+EOF : End Of File
+
+EOF is a special value that is defined in the header file <iostream>.
+
+When the user enters the EOF value, the loop will be terminated.
+
+detail of EOF-Controlled Loop
+
+    1. Declare a variable of int type.
+
+    2. Use the variable as the loop condition.
+
+    3. Read a value into the variable in the loop condition.
+
+    4. Change the value of the variable in the loop body.
+
+ex) while (std::cin >> number)
+
+    {
+
+        ...
+
+    }
+
+*/
+
+/* NOTE: Use a Flag
+
+Flag : a 'variable' that is used to control the loop.
+
+<detail of Flag>
+
+    1. Declare a variable of bool type.
+
+    2. Initialize the variable to false.
+
+    3. Use the variable as the loop condition.
+
+    4. Change the value of the variable in the loop body.
+
+ex) bool done = false; // done is a flag
+
+    while (!done)
+
+    {
+
+        ...
+
+        if (...)
+
+            done = true; //
+
+    }
+
+*/
+
+/* NOTE: For loop
+
+if There is no one among the three option, doesn't act.
+
+if you don't need initialization or update, don't write something but just semicolon.
+
+for (initialization; condition; update)
+
+ex) for (int i = 0; i < 10; i++)
+
+    {
+
+        ...
+
+    }
+
+*/
+   
+/* NOTE: How to use ternary operator
+#include <iostream>
 int main()
 {
+    int num1, num2;
+    int more_larger;
 
+    std::cout << "number 1: ";
+    std::cin >> num1;
+    std::cout << "number 2: ";
+    std::cin >> num2;
+    more_larger = (num1 > num2) ? num1 : num2; // 조건이 true면 more_larger = num1  <-->조건이 false면 more_larger = num2
+    std::cout << more_larger << " is more larger" << std::endl;
+    return 0;
+}
+*/
+
+/* NOTE: How to use switch statement
+switch : it looks like a 'if'
+#include <iostream>
+int main()
+{
+    int day;
+    std::cout << "Enter the number, 0 to 4";
+    std::cin >> day;
+    switch (day)
+    {
+    case 1:
+        std::cout << "GOOD DAY";
+        break; // break가 없으면 case 2가 실행이 되버린다.
+    case 2:
+        std::cout << "GOOD DAY2";
+        break;
+    case 3:
+        std::cout << "GOOD DAY3\n";
+        break;
+    case 4:
+        std::cout << "GOOD DAY4";
+        break;
+
+    default: // except같은 느낌으로 다른 것을 입력하면 이 결과가 출력.
+        std::cout << "NULL DAY";
+        break;
+    }
+    return 0;
+}
+*/
+
+/* NOTE: pointer(*)
+ pointer is a variable that stores the address of another variable.
+ declare pointer : type *var_name;
+ detail of pointer
+    1. pointer is a variable that stores the address of another variable.
+    |  ex) int *ptr; // ptr is a pointer variable that stores the address of int variable.
+    & : address of operator
+    * : dereference operator
+    ex) int *ptr; // ptr is a pointer variable that stores the address of int variable.
+        int var = 10; // var is a int variable that stores 10.
+        ptr = &var; // ptr stores the address of var.
+        cout << *ptr; // print the value of var.
+*/
+
+/* NOTE: What class is, How to use class with public and private
+
+
+#include <iostream>
+class Circle // "class" defines a class type :  class class_name { member_declaration; member_declaration; ... };
+{
+    private:    // access specifier. private members are accessible only from within other members of the same class.
+        double radius;
+    public:    // access specifier public members are accessible from anywhere where the object is visible.
+        Circle(double r) : radius(r) { }
+        double circum() { return 2 * radius * 3.14159265; }
+};
+
+int main()
+{
+    Circle foo(10.0);   // functional form
+    Circle bar = 20.0;  // assignment init.
+    Circle baz {30.0};  // uniform init.
+    Circle qux = {40.0};// POD-like
+
+    std::cout << "foo's circumference: " << foo.circum() << '\n'; //foo.circum() is a member function of class Circle.
     return 0;
 }
 
-/*
-    int x = -5;
-    //int abs(int x); -> absolute value of x
-
-    std::cout << abs(x) << std::endl; // this state is working.
-
-    std::cin >> x;
-    std::cout << abs(x) << std::endl; // this state is working.
-*/
-
-/*
-    int x = 5;
-    int y = 2;
-
-    //int pow(int x, int y); -> x raised to the power y
-
-    std::cout << pow(x, y) << std::endl; // this state is working.
-
-    std::cin >> x >> y;
-    std::cout << pow(x, y) << std::endl; // this state is working.
-*/
-
-/*
-    type cos(type x);                // cos(x) = cosine of x
-    type sin(type x);                // sin(x) = sine of x
-    type tan(type x);                // tan(x) = tangent of x
-    type acos(type x);               // acos(x) = arc cosine of x
-    type asin(type x);               // asin(x) = arc sine of x
-    type atan(type x);               // atan(x) = arc tangent of x
-    type atan2(type y, type x);      // atan2(y,x) = arc tangent of y/x
-    type cosh(type x);               // cosh(x) = hyperbolic cosine of x
-    type sinh(type x);               // sinh(x) = hyperbolic sine of x
-    type tanh(type x);               // tanh(x) = hyperbolic tangent of x
-    type exp(type x);                // exp(x) = ex
-    type log(type x);                // log(x) = natural logarithm of x
-    type log10(type x);              // log10(x) = base 10 logarithm of x
-    type pow(type x, type y);        // pow(x,y) = x raised to the power y
-    type sqrt(type x);               // sqrt(x) = square root of x
-    type ceil(type x);               // ceil(x) = x rounded up
-    type abs(type x);                // abs(x) = absolute value of x
-    type floor(type x);              // floor(x) = x rounded down
-    type fmod(type x, type y);       // fmod(x,y) = remainder of x/y
-    type ldexp(type x, int exp);     // ldexp(x,exp) = x*(2exp)
-    type frexp(type x, int *exp);    // frexp(x,exp) = x = y*(2exp)
-    type modf(type x, type * ipart); // modf(x,ipart) = x = *ipart + y
-    type erf(type x);                // erf(x) = error function of x
-    type erfc(type x);               // erfc(x) = 1 - erf(x)
-    type hypot(type x, type y);      // hypot(x,y) = sqrt(x2+y2)
-    type j0(type x);                 // j0(x) = Bessel function of the first kind, order zero  // Bessel function : 베셀 함수 =  상미분 방정식을 통해 기술되는 해를 구하는 함수.
-    type j1(type x);                 // j1(x) = Bessel function of the first kind, order one
-    type jn(int n, type x);          // jn(n,x) = Bessel function of the first kind, order n
-    type y0(type x);                 // y0(x) = Bessel function of the second kind, order zero
-    type y1(type x);                 // y1(x) = Bessel function of the second kind, order one
-    type yn(int n, type x);          // yn(n,x) = Bessel function of the second kind, order n
-    type erf(type x);                // erf(x) = error function of x
-    type erfc(type x);               // erfc(x) = 1 - erf(x)
-    type gamma(type x);              // gamma(x) = gamma function of x
-    type lgamma(type x);             // lgamma(x) = log(gamma(x))
 */
